7b512e7eb9589936b6055dbf478ee9b2893c06c0
srmt03 <98404525+srmt03@users.noreply.github.com>
2022-09-08 08:08:20 -0300

    Add files via upload

---
+++ aula11/modulos/estados.js
@@ -0,0 +1,91 @@
+/**********************************************
+ * Objetivo: Obter uma lista de estados 
+ * Data: 29/08
+ * Autor: Samuel Matos 
+ * Versao: 1.0
+ *********************************************/
+
+//Simulando o resultado de uma API
+var estados = 
+[
+    {
+        sigla: 'SP',
+        nome: 'Sao Paulo'
+    },
+    {
+        sigla: 'RJ',
+        nome: 'Rio de Janeiro'
+    },
+    {
+        sigla: 'AC',
+        nome: 'Acre'
+    },
+    {
+        sigla: 'BA',
+        nome: 'Bahia'
+    },
+    {
+        sigla: 'MS',
+        nome: 'Mato Grosso do Sul'
+    },
+    {
+        sigla: 'MT',
+        nome: 'Mato Grosso'
+    },
+    {
+        sigla: 'GO',
+        nome: 'Goias'
+    }
+];
+
+//Retorna todos os estados pela sigla 
+const getEstados = function () {
+    let listaEstados = [];
+    erro = true;
+
+    estados.forEach(item => {
+        listaEstados.push(item.sigla);
+        erro = false;
+    });
+    if (erro)
+        return false;
+    else
+        return listaEstados;
+};
+
+//Retorna os dados de um estado temdo como base a sigla 
+const getEstado = function (siglaEstado) {
+    let sigla = siglaEstado;
+    //Cria um obejto do tipo JSON
+    let estado = {};
+    let erro = true;
+
+    //
+    if (typeof(sigla) != 'undefined') 
+    {
+        //Tratamento para validacao de sigla vazia e com diferenca de caracteres 
+        if (sigla != '' && sigla.length == 2) 
+        {
+            estados.forEach (item => {
+                //Localiza um item no array (indexOf())
+                if (item.sigla.indexOf(sigla) == 0) 
+                {
+                    //Criamos as chaves 'uf' e 'descricao' para enviar para JSON
+                    estado.uf = item.sigla,
+                    estado.descricao = item.nome
+                    erro = false;
+                }   
+            });
+        }
+
+    }
+    if (erro)            
+            return false
+        else 
+            return estado 
+}
+//console.table(getEstados());
+//console.table(getEstado());
+console.log(getEstado('AC'));
+//console.log(getEstado('A'));
+//console.log(getEstado(''));
